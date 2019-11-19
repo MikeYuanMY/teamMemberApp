77cc4ddca50e9d039cc8def5d6295e1bf80235ea
MikeYuanMY <yuan.mike84@gmail.com>
2019-11-18 21:43:55 -0800

    added require validation for username and email

--- imports/ui/pages/login/login.js
+++ imports/ui/pages/login/login.js
@@ -1,3 +1,20 @@
+import { FlowRouter } from "meteor/kadira:flow-router";
+import { Meteor } from "meteor/meteor";
+import { jQuery } from "meteor/jquery";
+import { Template } from "meteor/templating";
+
 import "./login.html";
 
 //import "/public/assets/js/pages/base_pages_login.js";
+
+Template.login.helpers({
+  create: function() {},
+  rendered: function() {
+    $(".login").validate();
+  },
+  destroyed: function() {}
+});
+
+Template.login.events({
+  "click #foo": function(event, template) {}
+});
