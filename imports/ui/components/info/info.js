42214f4ad6c4131afa1ea087707fab2ede4558af
MikeYuanMY <yuan.mike84@gmail.com>
2019-11-21 14:33:32 -0800

    add component info.js

--- imports/ui/components/info/info.js
+++ imports/ui/components/info/info.js
@@ -1,32 +1,32 @@
-import { Links } from '/imports/api/links/links.js';
-import { Meteor } from 'meteor/meteor';
-import './info.html';
+import { Links } from "/imports/api/links/links.js";
+import { Meteor } from "meteor/meteor";
+import "./info.html";
 
-Template.info.onCreated(function () {
-  Meteor.subscribe('links.all');
+Template.info.onCreated(function() {
+  Meteor.subscribe("links.all");
 });
 
 Template.info.helpers({
   links() {
     return Links.find({});
-  },
+  }
 });
 
 Template.info.events({
-  'submit .info-link-add'(event) {
+  "submit .info-link-add"(event) {
     event.preventDefault();
 
     const target = event.target;
     const title = target.title;
     const url = target.url;
 
-    Meteor.call('links.insert', title.value, url.value, (error) => {
+    Meteor.call("links.insert", title.value, url.value, error => {
       if (error) {
         alert(error.error);
       } else {
-        title.value = '';
-        url.value = '';
+        title.value = "";
+        url.value = "";
       }
     });
-  },
+  }
 });
--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -1,1 +1,11 @@
 import "./body.html";
+
+import "../../../../public/assets/js/core/jquery.min.js";
+import "../../../../public/assets/js/core/bootstrap.min.js";
+import "../../../../public/assets/js/core/jquery.slimscroll.min.js";
+import "../../../../public/assets/js/core/jquery.scrollLock.min.js";
+import "../../../../public/assets/js/core/jquery.appear.min.js";
+import "../../../../public/assets/js/core/jquery.countTo.min.js";
+import "../../../../public/assets/js/core/jquery.placeholder.min.js";
+import "../../../../public/assets/js/core/js.cookie.min.js";
+//import "../../../../public/assets/js/app.js";
